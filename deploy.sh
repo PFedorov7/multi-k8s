e90089b26fa9d2b1ad0456ff10fbf59dbe1bda4c
Pavel Fedorov <pfedorov7721@gmail.com>
2021-03-21 18:23:10 +0300

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push pfedorov/multi-client:latest
+docker push pfedorov/multi-server:latest
+docker push pfedorov/multi-worker:latest
+
+docker push pfedorov/multi-client:$SHA
+docker push pfedorov/multi-server:$SHA
+docker push pfedorov/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=pfedorov/multi-server:$SHA
+kubectl set image deployments/client-deployment server=pfedorov/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=pfedorov/multi-worker:$SHA
